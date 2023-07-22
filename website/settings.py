cffd28f4c2b3c61e246eb8afa3e9fdf398d36847
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-07-22 16:55:05 +0530

    database host

--- website/settings.py
+++ website/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'chictux_final',
         'USER': 'root',
         'PASSWORD':'1234',
-        'HOST':'localhost',
+        'HOST':'%',
         'PORT':'3306',
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
