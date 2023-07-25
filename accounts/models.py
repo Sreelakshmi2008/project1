2b0ac4d50a67aa1ad88ac06b9149b6e457f0aa5a
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-07-25 20:05:29 +0530

    chamges in customuser

--- accounts/models.py
+++ accounts/models.py
@@ -11,23 +11,23 @@ from django.dispatch import receiver
 class CustomUserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""
 
-    def _create_user(self,email, name,phone_number,password=None, **extra_fields):
+    def _create_user(self,email,password=None, **extra_fields):
 
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name,phone_number=phone_number,**extra_fields)
+        user = self.model(email=email,**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email,name,phone_number,password=None, **extra_fields):
+    def create_user(self, email,password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email,name,phone_number,password, **extra_fields)
+        return self._create_user(email,password, **extra_fields)
 
-    def create_superuser(self, email,name,phone_number, password=None, **extra_fields):
+    def create_superuser(self, email,password=None, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -37,7 +37,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, name,phone_number,password, **extra_fields)
+        return self._create_user(email,password, **extra_fields)
 
 
 # custom user model for user accounts
