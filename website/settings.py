01aac91f9e9e405c7a0e6b1957beaecae4fa563f
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-07-26 16:03:24 +0530

    static changes

--- website/settings.py
+++ website/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/staticfiles/'
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
