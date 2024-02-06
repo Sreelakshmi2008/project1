5cd74f7ef9199b226b0888a6bf333a87a4281ba5
Sreelakshmi2008 <sreetb2008@gmail.com>
2024-02-06 12:28:12 +0530

    static

--- website/settings.py
+++ website/settings.py
@@ -130,12 +130,12 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'static')
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = BASE_DIR / "static"
 
 
 
