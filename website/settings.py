5f330896b13c959413844a31e3498886c611209e
Sreelakshmi2008 <sreetb2008@gmail.com>
2024-02-06 12:41:39 +0530

    stat

--- website/settings.py
+++ website/settings.py
@@ -132,9 +132,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_ROOT = BASE_DIR / "static"
 
 
